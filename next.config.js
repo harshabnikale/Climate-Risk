8a0897c1148bd5a96ec71fc59347992b08602e11
harshabnikale <harshanikale2015@gmail.com>
2023-05-02 00:22:38 +0530

    feat: edited next config file and api rount file papth

--- next.config.js
+++ next.config.js
@@ -1,6 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/:path*',
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        // Define the path to the CSV file
+        source: '/static/sample_data.csv',
+        // Set the headers to serve the file as CSV
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/csv',
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
