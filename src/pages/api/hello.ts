533b3b057c3d9c93824c4f82c1e215ca4762ee6e
harshabnikale <harshanikale2015@gmail.com>
2023-05-02 00:43:30 +0530

    fix: added path.cwd in api route

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,11 +2,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import fs from 'fs';
 import csvParser from 'csv-parser';
+import path from 'path';
 
-function getServerSideProps() {
+function getServerSideProps(file: any) {
   return new Promise((resolve, reject) => {
     const results: any = [];
-    fs.createReadStream('public/static/sample_data.csv')
+    fs.createReadStream(file)
       .pipe(csvParser())
       .on('data', (data: any) => {
         results.push(data);
@@ -25,10 +26,13 @@ export default async function handler(
   res: any
 ) {
   //  //load and parse data
- let data: any = await getServerSideProps();
- data.data.forEach((element: any) => {
-  Object.assign(element, { Location: element.Lat + ',' + element.Long, });
-  // a.push({location: element.Lat +','+ element.Long,})
-});
-  res.status(200).json(data.data);
+  const file = path.join(process.cwd(), 'public/static/sample_data.csv')
+ let data: any = await getServerSideProps(file);
+console.log(data);
+
+//  data.forEach((element: any) => {
+//   Object.assign(element, { Location: element.Lat + ',' + element.Long, });
+//   // a.push({location: element.Lat +','+ element.Long,})
+// });
+  res.status(200).json([data.data]);
 }
